42fbc3b608f41e988ba49401bfeca83226d14acd
Ransomwave <alexandrutudor2504@gmail.com>
2024-07-30 19:06:16 +0200

    test

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const CLIENT_SECRET = process.env.CLIENT_SECRET; // Replace with your own client secret
+const CLIENT_SECRET = "roblox"; // Replace with your own client secret
 // require("./bin/www");
 const express = require("express");
 const app = express();
@@ -14,9 +14,9 @@ app.post("/api/donations", (req, res) => {
   const { donorName, amount, clientSecret } = req.body; // Extract clientSecret from the request body
 
   // Validate the client secret
-  if (clientSecret !== CLIENT_SECRET) {
-    return res.status(401).send("Unauthorized: Invalid client secret"); // Return 418 status code if the client secret is nil or invalid
-  }
+  // if (clientSecret !== CLIENT_SECRET) {
+  // return res.status(401).send("Unauthorized: Invalid client secret"); // Return 418 status code if the client secret is nil or invalid
+  // }
 
   donations.push({ donorName, amount, timestamp: new Date() }); // Add timestamp for sorting
   res.status(200).send("Donation received");
