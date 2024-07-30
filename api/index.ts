11e669c182ce159780724448654ddc0bb134c63b
Ransomwave <alexandrutudor2504@gmail.com>
2024-07-30 18:18:08 +0200

    ,

--- api/index.ts
+++ api/index.ts
@@ -5,10 +5,7 @@ const path = require("path");
 app.use(express.json());
 
 // Serve static files from the 'public' directory
-// app.use(express.static("public"));
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 
 interface Donation {
   donorName: string;
