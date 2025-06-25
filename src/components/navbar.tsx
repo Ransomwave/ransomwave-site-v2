2e609c8c902f1beba48f866bab8ace811d2245a1
Ransomwave <63156992+Ransomwave@users.noreply.github.com>
2025-06-26 00:18:47 +0200

    Add everything: Add new components for ASCII banner, background, navbar, and splash text; include top background image and update Tailwind CSS configuration

---
+++ src/components/navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-center items-center p-4 gap-4 text-white">
+      <Link
+        href="/"
+        className="font-bold hover:text-shadow-[0_0_8px_rgba(255,255,255,0.5)]"
+      >
+        home
+      </Link>
+      <Link
+        href="/games"
+        className="font-bold hover:text-shadow-[0_0_8px_rgba(255,255,255,0.5)]"
+      >
+        games
+      </Link>
+      <Link
+        href="/projects"
+        className="font-bold hover:text-shadow-[0_0_8px_rgba(255,255,255,0.5)]"
+      >
+        projects
+      </Link>
+      <Link
+        href="/blog"
+        className="font-bold hover:text-shadow-[0_0_8px_rgba(255,255,255,0.5)]"
+      >
+        blog
+      </Link>
+    </div>
+  );
+}
