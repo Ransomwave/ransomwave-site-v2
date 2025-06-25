a70fe332cdb94f8cc3f4c810f8f35224ee88e466
Ransomwave <alexandrutudor2504@gmail.com>
2025-06-26 01:09:19 +0200

    Fix incorrect status code comment for unauthorized client secret validation

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.post("/api/donations", (req, res) => {
 
   // Validate the client secret
   if (clientSecret !== CLIENT_SECRET) {
-    return res.status(401).send("Unauthorized: Invalid client secret"); // Return 418 status code if the client secret is nil or invalid
+    return res.status(401).send("Unauthorized: Invalid client secret"); // Return 401 status code if the client secret is nil or invalid
   }
 
   // Process the donation
