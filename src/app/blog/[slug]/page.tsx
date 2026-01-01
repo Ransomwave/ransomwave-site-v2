5afc03f5e05f3c7cc248654c9fdac710e68ca813
Ransomwave <63156992+Ransomwave@users.noreply.github.com>
2026-01-01 01:36:47 +0100

    feat(blog): enhance blog post metadata generation and improve styling in BlogCard component

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,8 +3,34 @@ import path from "path";
 import matter from "gray-matter";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 const postsDirectory = path.join(process.cwd(), "src", "content", "blog");
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+  const filePath = path.join(postsDirectory, `${slug}.mdx`);
+
+  if (!fs.existsSync(filePath)) {
+    return {
+      title: "Blog Post - Ransomwave's Games",
+      description: "Read the latest blog post from Ransomwave's Games.",
+    };
+  }
+
+  const source = fs.readFileSync(filePath, "utf8");
+  const { data } = matter(source);
+
+  return {
+    title: `${data.title} - Blog`,
+    description:
+      data.description ?? "Read the latest blog post from Ransomwave's Games.",
+  };
+}
 
 export async function generateStaticParams() {
   if (!fs.existsSync(postsDirectory)) return [];
@@ -33,6 +59,33 @@ export default async function BlogPost({
     notFound();
   }
 
+  const mdxComponents = {
+    h1: (props: React.ComponentProps<"h1">) => (
+      <h1 className="mt-6 mb-3 text-2xl " {...props} />
+    ),
+    h2: (props: React.ComponentProps<"h2">) => (
+      <h2 className="mt-6 text-xl text-gray-400" {...props} />
+    ),
+    h3: (props: React.ComponentProps<"h3">) => (
+      <h3 className="mt-6 mb-3 text-lg" {...props} />
+    ),
+    p: (props: React.ComponentProps<"p">) => (
+      <p className="mb-4 leading-7" {...props} />
+    ),
+    ul: (props: React.ComponentProps<"ul">) => (
+      <ul className="list-disc list-inside mb-4" {...props} />
+    ),
+    ol: (props: React.ComponentProps<"ol">) => (
+      <ol className="list-decimal list-inside mb-4" {...props} />
+    ),
+    li: (props: React.ComponentProps<"li">) => (
+      <li className="mb-2" {...props} />
+    ),
+    a: (props: React.ComponentProps<"a">) => (
+      <a className="text-red-500 underline" {...props} />
+    ),
+  };
+
   const source = fs.readFileSync(filePath, "utf8");
   const { content, data } = matter(source);
 
@@ -40,7 +93,9 @@ export default async function BlogPost({
     <article>
       <h1 className="text-center md:text-[1.2vw] mb-[1%]">{data.title}</h1>
       <p className="text-center md:text-[1vw] mb-[2%]">{data.date}</p>
-      <MDXRemote source={content} />
+      <div className="mx-[5%]">
+        <MDXRemote source={content} components={mdxComponents} />
+      </div>
     </article>
   );
 }
--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -19,7 +19,7 @@ export default function BlogCard({
   date,
 }: BlogCardProps) {
   return (
-    <div className="border border-[#ffffff62] bg-[rgba(0,0,0,0.3)] hover:bg-[rgba(23,23,23,0.3)] backdrop-blur-xs rounded-md shadow-[black_0_0_15px] overflow-hidden">
+    <div className="border border-[#ffffff62] bg-[rgba(0,0,0,0.3)] hover:bg-[rgba(23,23,23,0.3)] backdrop-blur-xs rounded-md shadow-[black_0_0_15px] overflow-hidden mb-5">
       <Link href={link} className="">
         {imageUrl && (
           <div className="relative w-full h-48 overflow-hidden">
@@ -33,7 +33,7 @@ export default function BlogCard({
           </div>
         )}
         <div className="p-4 flex flex-col h-full">
-          <h2 className="text-xl font-bold mb-2 text-white">{title}</h2>
+          <h2 className="text-xl mb-2 text-white">{title}</h2>
           <p className="text-sm text-gray-300 mb-4 flex-grow">{description}</p>
           <p className="text-xs text-gray-400 mt-auto">
             {new Date(date).toLocaleDateString()}
